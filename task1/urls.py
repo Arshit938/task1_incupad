13e93a7d00e5c17b12096b7335703871314d26f1
Arshit <127618511+Arshit938@users.noreply.github.com>
2024-08-30 08:24:06 +0530

    Update urls.py

--- task1/urls.py
+++ task1/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from projectApi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat',views.processChat,name='chat_endpoint'),
+    path('submitbutton',views.submitButton,name='submitbutton_endpoint'),
+    path('submitform',views.submitForm,name='submitform_endpoint'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
