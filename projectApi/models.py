e166b2f9e9ce2f21a82d22b533a6372481be1e91
Arshit <127618511+Arshit938@users.noreply.github.com>
2024-09-01 10:46:45 +0530

    Update models.py

--- projectApi/models.py
+++ projectApi/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
 class docTable(models.Model):
+    id=models.IntegerField(primary_key=True)
     label=models.CharField(max_length=100,default='')#file name
     doc_file=models.FileField(upload_to='doc_templates/')
+
+    def __str__(self) -> str:
+        return str(self.id)
+    
+class formLabels(models.Model):
+    id=models.IntegerField(primary_key=True)
     doc_fields=models.JSONField(default=dict)
 
     def __str__(self) -> str:
         return str(self.id)
-    
-class newPdfUpload(models.Model):
-    name=models.TextField()
-    doc_file=models.FileField(upload_to='filled_pdfs/')
-
-    def __str__(self) -> str:
-        return str(self.id)
-    
-
